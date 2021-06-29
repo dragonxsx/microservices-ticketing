a9d55ccb9037fc72fa30e026749464fb946ca7a9
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2021-06-29 07:06:59 +0700

    Init SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,22 @@
-const Main = () => {
+import axios from "axios";
+
+const LandingPage = ({currentUser}) => {
+    console.log('I am in component', currentUser);
     return <h1>Landing Page</h1>;
 }
 
-export default Main;
+LandingPage.getInitialProps = async () => {
+    const response = await axios.get('/api/users/currentuser');
+
+    if(typeof  window === 'undefined'){
+        // we are on the server!
+        // requests should be made to http://ingress-nginx
+    } else {
+        // we are on the browser!
+        // requests can be made with a base url of ''
+    }
+
+    return {};
+};
+
+export default LandingPage;
