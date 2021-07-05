3b1b6f03c30ce4aef0718e12d26d8af6f028b2bf
LongNT <longnt@ql6625.fr>
2021-07-05 09:20:59 +0700

    GetInitialProps: Request on server and client
    ServerSide: Access through ingress-nginx, specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,18 +5,26 @@ const LandingPage = ({currentUser}) => {
     return <h1>Landing Page</h1>;
 }
 
-LandingPage.getInitialProps = async () => {
-    const response = await axios.get('/api/users/currentuser');
-
+LandingPage.getInitialProps = async ({req}) => {
     if(typeof  window === 'undefined'){
         // we are on the server!
         // requests should be made to http://ingress-nginx
+        // http://SERVICENAME.NAMESPACE.svc.cluster.local
+        const {data} = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+                headers: req.headers,
+                // headers: {host: ticketing.dev }
+            }
+        );
+
+        return data;
     } else {
         // we are on the browser!
         // requests can be made with a base url of ''
-    }
+        const { data } = await axios.get('/api/users/currentuser');
 
-    return {};
+        return data;
+    }
 };
 
 export default LandingPage;
