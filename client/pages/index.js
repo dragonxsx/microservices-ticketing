10c605533c951d1c5b6d67323c3a8b83ec4e33cc
LongNT <longnt@ql6625.fr>
2021-07-06 08:33:04 +0700

    Add Sign In form

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,7 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({currentUser}) => {
-    console.log('I am in component', currentUser);
-    return <h1>Landing Page</h1>;
+    return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
 LandingPage.getInitialProps = async (context) => {
