44b8e46a99c3649e4b483251cdc3010f4ef603e4
LongNT <longnt@ql6625.fr>
2021-07-08 18:00:15 +0700

    Add ticket creating functionality

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import {errorHandler, NotFoundError} from '@banana.inc/common';
+import {currentUser, errorHandler, NotFoundError} from '@banana.inc/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,9 @@ app.use(
         secure: process.env.NODE_ENV !== 'test'
     })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res, next) => {
     throw new NotFoundError();
