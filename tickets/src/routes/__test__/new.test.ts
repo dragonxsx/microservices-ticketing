3180c64d0f77373ad3cc93d037578369613c64b8
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2021-07-08 21:21:54 +0700

    Test the authenticated user request

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -17,6 +17,14 @@ it('can only be accessed if the user is signed in', async () => {
         .expect(401);
 });
 
+it('returns a status other than 401 if the user is signed in', async () => {
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({});
+
+    expect(response.status).not.toEqual(401);
+});
 
 it('returns an error if an invalid title is provided', async () => {
     
