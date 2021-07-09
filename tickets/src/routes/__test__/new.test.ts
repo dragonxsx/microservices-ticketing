692bf90ee49e299487fc5cb62c805c7f35a1ee88
LongNT <longnt@ql6625.fr>
2021-07-09 18:19:33 +0700

    Complete Ticket creating route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
     const response = await request(app)
@@ -60,12 +61,22 @@ it('returns an error if an invalid price is provided', async () => {
 
 it('creates a ticket with valid inputs', async () => {
     // add in a check to make sure a ticket was saved
+    let tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(0);
 
+    const title = 'asdasdasdasc3';
+    const price = 20;
     await request(app)
         .post('/api/tickets')
+        .set('Cookie', global.signin())
         .send({
-            title: 'asdasdasdasc3',
-            price: 20
+            title: title,
+            price: price
         })
         .expect(201);
+
+    tickets = await Ticket.find({});
+    expect(tickets.length).toEqual(1);
+    expect(tickets[0].title).toEqual(title);
+    expect(tickets[0].price).toEqual(price);
 });
