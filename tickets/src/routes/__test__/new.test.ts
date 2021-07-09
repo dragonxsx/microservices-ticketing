0789ef4a094f11b6005b68d0d8bc322fc0ee184c
LongNT <longnt@ql6625.fr>
2021-07-09 16:01:53 +0700

    Add ticket creating router validation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -27,15 +27,45 @@ it('returns a status other than 401 if the user is signed in', async () => {
 });
 
 it('returns an error if an invalid title is provided', async () => {
-    
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: '',
+            price: 10
+        })
+        .expect(400);
 });
 
 
 it('returns an error if an invalid price is provided', async () => {
-    
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'asdfwasd',
+            price: -10
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'asdfwasd'
+        })
+        .expect(400);
 });
 
 
 it('creates a ticket with valid inputs', async () => {
-    
+    // add in a check to make sure a ticket was saved
+
+    await request(app)
+        .post('/api/tickets')
+        .send({
+            title: 'asdasdasdasc3',
+            price: 20
+        })
+        .expect(201);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,18 @@
-import { requireAuth } from '@banana.inc/common';
 import express, {Request, Response} from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@banana.inc/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
+router.post('/api/tickets', requireAuth, [
+    body('title')
+        .not()
+        .isEmpty()
+        .withMessage('Title is required'),
+    body('price')
+        .isFloat({gt: 0})
+        .withMessage('Price must be greater than 0')
+], validateRequest, (req: Request, res: Response) => {
     res.sendStatus(200);
 });
 
