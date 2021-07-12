daf70ffe4214a55beae4ca754c50eaf48ef7c8a0
LongNT <longnt@ql6625.fr>
2021-07-12 10:37:53 +0700

    Add updateTicketRouter

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,103 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
+
+it('returns a 404 if the provided id does not exist', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app)
+        .put(`/api/tickets/${id}`)
+        .set('Cookie', global.signin())
+        .send({
+            title: 'fdfjdf',
+            price: 20
+        })
+        .expect(404);
+});
+
+it('returns a 401 if the user is not authenticated', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+    await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+        title: 'fdfjdf',
+        price: 20
+    })
+    .expect(401);
+});
+
+it('returns a 401 if the user does not own the ticket', async () => {
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: 'ajnjev',
+            price: 56
+        });
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', global.signin())
+        .send({
+            title: 'sjgje4',
+            price: 6734
+        })
+        .expect(401);
+});
+
+it('returns a 400 if the user provides an invalid title or price', async () => {
+    const cookie = global.signin();
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'asdsverf',
+            price: 351
+        });
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: 351
+        })
+        .expect(400);
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: -10
+        })
+        .expect(400);
+});
+
+it('updates the ticket provided valid inputs', async () => {
+    const cookie = global.signin();
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'asdsverf',
+            price: 351
+        });
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'New Title',
+            price: 351
+        })
+        .expect(200);
+    
+        const ticketResponse = await request(app)
+            .get(`/api/tickets/${response.body.id}`)
+            .send();
+
+        expect(ticketResponse.body.title).toEqual('New Title');
+        expect(ticketResponse.body.price).toEqual(351);
+});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -42,7 +42,7 @@ afterAll(async () => {
 global.signin = () => {
     // Build a JWT payload {id, email}
     const payload = {
-        id: '123kajsdskk3',
+        id: new mongoose.Types.ObjectId().toHexString(),
         email: 'test@test.com'
     };
 
