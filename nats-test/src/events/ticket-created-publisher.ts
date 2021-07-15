b8943da193cfbb7fc654753f208cfbbf8b7c78b4
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2021-07-15 21:19:50 +0700

    Add custom Publisher

---
+++ nats-test/src/events/ticket-created-publisher.ts
@@ -0,0 +1,8 @@
+import { Publisher } from "./base-publisher";
+import { Subjects } from "./subjects";
+import { TicketCreatedEvent } from "./ticket-created-event";
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+    readonly subject = Subjects.TicketCreated;
+
+}
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,6 @@
 import nats from 'node-nats-streaming';
 import {randomBytes} from 'crypto';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -10,13 +11,10 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Publisher connected to NATS');
 
-    const data = JSON.stringify({
+    const publisher = new TicketCreatedPublisher(stan);
+    publisher.publish({
         id: '123',
         title: 'concert',
         price: 20
     });
-
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    })
 })
