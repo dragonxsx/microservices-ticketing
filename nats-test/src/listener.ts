7ce2db4f74fab7bdd36a9a0513808f92e9cb08cc
LongNT <longnt@ql6625.fr>
2021-07-15 10:03:21 +0700

    Add TicketCreatedListener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,29 +15,7 @@ stan.on('connect', () => {
         process.exit();
     })
     
-    const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-    
-    const subscription = stan.subscribe(
-        'ticket:created', 
-        'orders-service-queue-group',
-        options
-    );
-
-    subscription.on('message', (msg: Message) => {
-        const data = msg.getData();
-
-        if(typeof data === 'string') {
-            console.log(
-                `Received event #${msg.getSequence()}, with data ${data}`
-            );
-        }
-
-        msg.ack();
-    });
+    new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -85,5 +63,16 @@ abstract class Listener {
         return typeof data === 'string'
             ? JSON.parse(data)                      // String
             : JSON.parse(data.toString('utf8'));    // Buffer
+    }
+}
+
+class TicketCreatedListener extends Listener {
+    subject = 'ticket:created';
+    queueGroupName = 'payments-service';
+
+    onMessage(data: any, msg: Message) {
+        console.log('Event data!', data);
+
+        msg.ack();
     }
 }
