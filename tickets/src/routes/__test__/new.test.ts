43c41cb76d04fcf5d0911fbbb7138a17d4def6f1
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2021-07-16 23:01:16 +0700

    Use jest.Mock() on Publisher to ensuring Mock Invocations

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+import {natsWrapper} from '../../nats-wrapper';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
     const response = await request(app)
@@ -80,3 +81,18 @@ it('creates a ticket with valid inputs', async () => {
     expect(tickets[0].title).toEqual(title);
     expect(tickets[0].price).toEqual(price);
 });
+
+it('publishes an event', async () => {
+    const title = 'asdasdasdasc3';
+    const price = 20;
+    await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title: title,
+            price: price
+        })
+        .expect(201);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+})
--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import request from "supertest";
 import { app } from "../../app";
+import {natsWrapper} from "../../nats-wrapper"
 
 it('returns a 404 if the provided id does not exist', async () => {
     const id = new mongoose.Types.ObjectId().toHexString();
@@ -100,4 +101,27 @@ it('updates the ticket provided valid inputs', async () => {
 
         expect(ticketResponse.body.title).toEqual('New Title');
         expect(ticketResponse.body.price).toEqual(351);
+});
+
+it('publishes an event', async () => {
+    const cookie = global.signin();
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'asdsverf',
+            price: 351
+        });
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'new title',
+            price: 351
+        })
+        .expect(200);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
