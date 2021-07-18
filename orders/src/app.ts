6364c736386c3222e2668fa7387ecdb00f61fcd2
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2021-07-18 22:14:10 +0700

    Add Orders routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import {currentUser, errorHandler, NotFoundError} from '@banana.inc/common';
+import { indexOrderRouter } from './routes';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
+import { deleteOrderRouter } from './routes/delete';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +18,11 @@ app.use(
     })
 );
 app.use(currentUser);
+
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req, res, next) => {
     throw new NotFoundError();
