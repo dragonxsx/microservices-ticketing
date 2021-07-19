d5c66ac9977cce171b1be475c261b3ac912137f6
Nguyen Thanh Long <dragon.sunshine@gmail.com>
2021-07-19 23:43:10 +0700

    Update Order Creation route logic

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,6 @@
+import { OrderStatus } from '@banana.inc/common';
 import mongoose from 'mongoose';
+import { Order } from './order';
 
 interface TicketAttrs {
     title: string;
@@ -8,6 +10,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
+    isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -35,6 +38,22 @@ const ticketSchema = new mongoose.Schema({
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
+}
+
+ticketSchema.methods.isReserved = async function () {
+    // this === the ticket document that we just called 'isReserved' on
+    const existingOrder = await Order.findOne({
+        ticket: this.id,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete
+            ]
+        }
+    });
+
+    return !!existingOrder;
 }
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
