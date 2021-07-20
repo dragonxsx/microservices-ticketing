abe6470262adb7a704a4992ee85f077ba075c9a1
LongNT <longnt@ql6625.fr>
2021-07-20 12:44:18 +0700

    Add Orders new router test

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,96 @@
+import { OrderStatus } from '@banana.inc/common';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import {app} from '../../app';
+import { Order } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+
+it('has a route handler listening to /api/tickets for post request', async () => {
+    const response = await request(app)
+        .post('/api/orders')
+        .send({});
+
+    expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed if the user is signed in', async () => {
+    await request(app)
+        .post('/api/orders')
+        .send()
+        .expect(401);
+});
+
+it('returns a status other than 401 if the user is signed in', async () => {
+    const response = await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send();
+    
+    expect(response.status).not.toEqual(401);
+});
+
+it('returns an error if an invalid ticketId is provided', async () => {
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId : 0})
+        .expect(400);
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId : ''})
+        .expect(400);
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId : 'ABCXYZ'})
+        .expect(400);
+});
+
+it('returns an error if the ticket does not exist', async () => {
+    const ticketId = mongoose.Types.ObjectId();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId })
+        .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    })
+    await ticket.save();
+
+    const order = Order.build({
+        ticket,
+        userId: '123123334ffsc',
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(400);
+});
+
+it('reserves a ticket', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(201);
+});
