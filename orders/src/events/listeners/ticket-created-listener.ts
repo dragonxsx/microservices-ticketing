885d319797883b0cc426c8238279a992e93fe926
LongNT <longnt@ql6625.fr>
2021-07-22 13:51:32 +0700

    Add TicketCreateListener

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, Subjects, TicketCreatedEvent } from "@banana.inc/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject = Subjects.TicketCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+        const { id, title, price } = data;
+
+        const ticket = await Ticket.build({
+            id,
+            title,
+            price
+        });
+        await ticket.save();
+
+        msg.ack();
+    }
+}
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import { Order } from './order';
 
 interface TicketAttrs {
+    id: string;
     title: string;
     price: number;
 }
@@ -37,7 +38,11 @@ const ticketSchema = new mongoose.Schema({
 });
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-    return new Ticket(attrs);
+    return new Ticket({
+        _id: attrs.id,
+        title: attrs.title,
+        price: attrs.price
+    });
 }
 
 ticketSchema.methods.isReserved = async function () {
