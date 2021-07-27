d1a594f50ee0e88b7478665a678f422106d2061b
LongNT <longnt@ql6625.fr>
2021-07-27 10:14:56 +0700

    Setup stripe integration

--- commands.sh
+++ commands.sh
@@ -5,3 +5,5 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ssl-key.key -out ssl
 
 kubectl create secret tls ${CERT_NAME} --key ${KEY_FILE} --cert ${CERT_FILE}
 kubectl create secret tls ticketing --key ssl-key.key --cert ssl-cert.cert
+
+kubectl create secret generic stripe-secret --from-literal STRIPE_KEY=sk_test_51JHfUpBA28lmwzEchTJIt7zeUyh1GEVzY9iXTPdsEkzwmCAAId36yPWL4rz7dB9WzF6250rfDUgFKPMGGrq42rWn00aEgwelbN
