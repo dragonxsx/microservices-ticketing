d9b36444554355e88ad24797d0ec81227ef30410
LongNT <longnt@ql6625.fr>
2021-07-30 10:43:38 +0700

    Add startup messages

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,8 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
+    console.log("Starting ...");
+
     if(!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
